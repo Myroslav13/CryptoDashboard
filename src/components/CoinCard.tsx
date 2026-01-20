5f1581e908b47c1cf9dbd483ac8bde378403fc0f
Myroslav13 <myroslavshchuruk@gmail.com>
2026-01-20 12:27:30 +0200

    CoinCard added

---
+++ src/components/CoinCard.tsx
@@ -0,0 +1,20 @@
+import "../App.css";
+import type { CryptoToken } from "../CryptoToken";
+
+interface CoinCardProps {
+   data: CryptoToken;
+}
+
+function CoinCard({ data }: CoinCardProps) {
+   return (
+      <div className="card" style={{width: '10rem'}}>
+         <img className="card-img-top" src={data.image} alt="Crypto token image"/>
+         <div className="card-body border-top">
+            <h5 className="card-title">{data.name}</h5>
+            <p className="card-text">{data.current_price}</p>
+         </div>
+      </div>
+   );
+}
+
+export default CoinCard;
